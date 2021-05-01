218b9b74ccce8f8087d59d85cb9f5dbb3d9081a4
Debajyoti Datta <30590895+dattadebajyoti@users.noreply.github.com>
2021-05-01 00:10:29 -0300

    Update sliding_window.cpp

--- sliding_window.cpp
+++ sliding_window.cpp
@@ -2,31 +2,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int maxSum(int arr[], int n, int k)
-{
-    if (n < k) {
-        cout << "Invalid";
-        return -1;
-    }
- 
-    int max_sum = 0;
-    for (int i = 0; i < k; i++)
-        max_sum += arr[i];
- 
-    int window_sum = max_sum;
-    for (int i = k; i < n; i++) {
-        window_sum += arr[i] - arr[i - k];
-        max_sum = max(max_sum, window_sum);
-    }
- 
-    return max_sum;
-}
+
 
 int main()
 {
-    int arr[] = { 1, 4, 2, 10, 2, 3, 1, 0, 20 };
-    int k = 4;
+    int arr[] = { 1, 4, 2, 10, 23, 3, 1, 0, 20};
     int n = sizeof(arr) / sizeof(arr[0]);
-    cout << maxSum(arr, n, k);
+    
+    int window_size = 4;
+    
+    int max_sum = 0;
+    for(int i = 0; i < window_size; i++)
+    {
+        max_sum = max_sum + arr[i];
+    }
+    
+    int window_sum = max_sum;
+    
+    for(int i = window_size; i < n; i++)
+    {
+        window_sum += arr[i] - arr[i - window_size];
+        max_sum = max(max_sum, window_sum);
+    }
+    
+    
+    cout<< max_sum<<endl;
+ 
     return 0;
 }
